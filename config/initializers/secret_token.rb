046878562c75e20cf3f3c2b6f78b8a4a5e6b3be9
Freddie Vargus <freddie@quantopian.com>
2016-08-04 19:59:02 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BufcApp::Application.config.secret_token = '886e9048708a10e5c07ea019019903b1eb8c464c0765257bd2f90629c5a01e02974bea3c0ae303297df3b111ad44a630d519dc3b5a397cd50b1b4b921d00fcf9'
