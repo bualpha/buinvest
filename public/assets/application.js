d4f5c72e2c56c177ad55dae3af0e1b34aa613a28
Freddie Vargus <fv4.programmer@gmail.com>
2016-08-22 23:14:59 -0400

    MAINT: Assets

--- public/assets/application.js
+++ public/assets/application.js
@@ -12795,6 +12795,10 @@ return jQuery;
 
 
 }).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
