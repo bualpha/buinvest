c5d9052ad189278116d6b21a92229a5ef7f787f3
Freddie Vargus <fvargus4@bu.edu>
2016-09-14 00:37:30 -0400

    ENH: Add stock pitches to resource page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,20 @@ BufcApp::Application.routes.draw do
   get "/EthicsStatement91115.pdf" => "portfolio#ethics"
   get "/BUFCSecuritySelectionProcess.pdf" => "portfolio#security_selection"
 
+  get "/AliNawazPhilBarcelona.pdf" => "resources#pres1"
+  get "/AnishGuha.pdf" => "resources#pres2"
+  get "/BrianMalone.pdf" => "resources#pres3"
+  get "/ChrisPratt.pdf" => "resources#pres4"
+  get "/ColeSecor.pdf" => "resources#pres5"
+  get "/DenisVisnjic.pdf" => "resources#pres6"
+  get "/JakeStrzalkowski.pdf" => "resources#pres7"
+  get "/JustinChiu.pdf" => "resources#pres8"
+  get "/KeshavKakkar.pdf" => "resources#pres9"
+  get "/MajidMowlood.pdf" => "resources#pres10"
+  get "/MattLeung.pdf" => "resources#pres11"
+  get "/MattPostorino.pdf" => "resources#pres12"
+  get "/SantoshMorasa.pdf" => "resources#pres13"
+  get "/UrjaLakhani.pdf" => "resources#pres14"
 
   resources :main do
     collection do
