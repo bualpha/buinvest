cd6c55b5ec488c1998501ae1861cceecd29db5d5
Freddie Vargus <fvargus4@bu.edu>
2017-09-09 20:49:22 -0400

    MAINT: Remove check for rails env

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,23 +3,21 @@ class ResourcesController < ApplicationController
     @resources = {}
     @presentations = {}
 
-    if Rails.env.production?
-      im_bucket = IM_S3_BUCKET.objects
+    im_bucket = IM_S3_BUCKET.objects
 
-      im_bucket.each do |file|
-        @resources["#{file.key}".gsub('.pdf', '')] = "#{file.public_url}"
-      end
+    im_bucket.each do |file|
+      @resources["#{file.key}".gsub('.pdf', '')] = "#{file.public_url}"
+    end
 
-      ir_bucket = IR_S3_BUCKET.objects
+    ir_bucket = IR_S3_BUCKET.objects
 
-      ir_bucket.each do |file|
-        @presentations["#{file.key}".gsub('.pdf', '').gsub(/(?<=[a-z])(?=[A-Z])/, ' ')] = "#{file.public_url}"
-      end
+    ir_bucket.each do |file|
+      @presentations["#{file.key}".gsub('.pdf', '').gsub(/(?<=[a-z])(?=[A-Z])/, ' ')] = "#{file.public_url}"
     end
 
     # XXX: Come up with some better interface to the quant-library resources
     @quant = {}
-    @quant["BU Alpha quant-library"] = "https://github.com/bualpha/quant-library"
+    @quant["quant-library"] = "https://github.com/bualpha/quant-library"
     @quant["Quantopian Lecture Series"] = "https://www.quantopian.com/lectures"
   end
 end
